86a3bbffc6ddf8ee5ca4bea87422692f63b5c8db
Aditya Gupta <115105977+Aditya95205@users.noreply.github.com>
2025-07-13 06:07:33 +0530

    Add files via upload

---
+++ JQuery/index.js
@@ -0,0 +1,20 @@
+// $("h1").addClass("big-title");
+
+// $("h1").text("Hey");
+
+// console.log($("img").attr("src"));\
+
+// $("button").click(function(){
+// $("h1").css("color","purple");
+// });
+
+// $("input").keypress(function(event){
+//     console.log(event.key);
+//     $("h1").text(event.key);
+// });
+
+$("h1").on("mouseover",function()
+{
+    $("h1").css("color","purple")
+}
+);
